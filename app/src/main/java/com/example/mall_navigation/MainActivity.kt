27d41bb6ab9a8fd1e300c566c39926ad45ceb191
Arsen <dyachkov.a.a@edu.mirea.ru>
2026-05-26 19:47:44 +0300

    Убраны лишние зависимости

--- app/src/main/java/com/example/mall_navigation/MainActivity.kt
+++ app/src/main/java/com/example/mall_navigation/MainActivity.kt
@@ -14,9 +14,7 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.coroutines.launch
-import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
