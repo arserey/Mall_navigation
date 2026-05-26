1a3cb51caf9de255f08bc4c45a12bf46ca7cddad
Arsen <dyachkov.a.a@edu.mirea.ru>
2026-05-26 18:56:58 +0300

    Добавлены классы ApiService.kt (у которого какие-то красные линии), RetrofitClient.kt, и начат NavigationViewModel.kt

---
+++ app/src/main/java/com/example/mall_navigation/network/RetrofitClient.kt
@@ -0,0 +1,28 @@
+package com.example.mall_navigation.network
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+    // Для эмулятора Android
+    private const val BASE_URL = "http://10.0.2.2:8080/"
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
---
+++ app/src/main/java/com/example/mall_navigation/viewmodel/NavigationViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.mall_navigation.viewmodel
+
+class NavigationViewModel {
+}
