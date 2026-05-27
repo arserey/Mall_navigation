c553f191e91e82391463d861c857dd96be547beb
Arsen <dyachkov.a.a@edu.mirea.ru>
2026-05-27 17:09:59 +0300

    Вправлен ApiService.kt

--- app/src/main/java/com/example/mall_navigation/network/ApiService.kt
+++ app/src/main/java/com/example/mall_navigation/network/ApiService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-class ApiService {
+interface ApiService {
     @GET("shops")
     suspend fun getAllShops(): List<Shop>
 
